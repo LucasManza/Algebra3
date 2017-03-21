bf0492d20ca6a8e88cb5bdd62ba0b6bc7d225c81
Lucas <lucas.manzanelli@ing.austral.edu.ar>
2017-03-21 00:10:54 -0300

    Ejercicio 7

--- source/Ejercicio7.java
+++ source/Ejercicio7.java
@@ -4,27 +4,26 @@ import java.util.ArrayList;
  * Created by Lucas on 20/3/2017.
  */
 public class Ejercicio7 {
-//    public static int MCD(int a, int b) {
-//        ArrayList factoresPrimosA = Ejercicio6.factoresPrimos(a);
-//        ArrayList factoresPrimosB = Ejercicio6.factoresPrimos(b);
-//
-//        ArrayList result = new ArrayList();
-//
-//        if (factoresPrimosA.size() >= factoresPrimosB.size()) {
-//            for (int i = 0; i < factoresPrimosB.size(); i++) {
-//                if (factoresPrimosA.contains(factoresPrimosB.get(i)))
-//                    result.add(factoresPrimosB.get(i));
-//            }
-//        } else {
-//            for (int i = 0; i < factoresPrimosA.size(); i++) {
-//                if (factoresPrimosB.contains(factoresPrimosA.get(i)))
-//                    result.add(factoresPrimosA.get(i));
-//            }
-//
-//        }
-//
-//
-//    }
+    public static int MCD(int a, int b) {
+        ArrayList factoresPrimosA = factoreo(a);
+        ArrayList factoresPrimosB = factoreo(b);
+
+        int result=0;
+
+        if(factoresPrimosA.size()>=factoresPrimosB.size()) {
+            for (int i = 0; i <factoresPrimosA.size();i++){
+                if(factoresPrimosB.contains(factoresPrimosA.get(i)) && i!=(i+1))
+                result +=contadorMenorComun(factoresPrimosA,factoresPrimosB,i);
+            }
+        }
+        else {
+            for (int i = 0; i < factoresPrimosB.size(); i++) {
+                if (factoresPrimosA.contains(factoresPrimosB.get(i)) && i != (i + 1))
+                    result += contadorMenorComun(factoresPrimosA, factoresPrimosB, i);
+            }
+        }
+        return result;
+    }
 
     public static ArrayList factoreo(int a){
         ArrayList result = new ArrayList();
@@ -32,12 +31,39 @@ public class Ejercicio7 {
         int i = 2;
         int j = a;
 
-        while (i<j){
+        while (i<=j){
             if(Ejercicio6.numeroPrimo(i)  &&  j%i ==0){
                 result.add(i);
                 j= j/i;
             }else{
                 i++;
+            }
+        }
+        return result;
+    }
+
+    public static int contadorMenorComun(ArrayList a, ArrayList b, int z){
+        ArrayList contadorA = new ArrayList();
+        ArrayList contadorB = new ArrayList();
+        int result =0;
+
+        for(int i=0; i<a.size();i++){
+            if(a.get(i)==z){
+                contadorA.add(a.get(i));
+            }
+        }
+        for(int j=0; j<b.size();j++){
+            if(b.get(j)==z){
+                contadorB.add(b.get(j));
+            }
+        }
+        if(contadorA.size()<=contadorB.size()) {
+            for (int ai=0;ai<contadorA.size();ai++){
+                result+=contadorA.get(ai);
+            }
+        }else {
+            for (int ai = 0; ai < contadorA.size(); ai++) {
+                result += contadorA.get(ai);
             }
         }
         return result;
