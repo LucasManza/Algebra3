d5a839be45bad252fa698601443875ad6d56bea3
Lucas <lucas.manzanelli@ing.austral.edu.ar>
2017-04-29 20:07:58 -0300

    EJERCICIO4 WITH RECURSION DONE!

--- source/Guia1/Ejercicio5.java
+++ source/Guia1/Ejercicio5.java
@@ -17,4 +17,18 @@ public class Ejercicio5 {
         }
         return false;
     }
+
+    public static boolean perteneceAlCojuntoRecursion(int[] array, int a){
+        return perteneceAlConjunto(0,array,a);
+    }
+
+    private static boolean perteneceAlConjunto(int i, int[] array, int a) {
+        if(i<array.length){
+            if(array[i]==a){
+                return true;
+            }
+            perteneceAlConjunto(i+1,array,a);
+        }
+        return false;
+    }
 }
