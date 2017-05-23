b9f75f621423ee3e6260079bd62de0df8660e2fd
Lucas <lucas.manzanelli@ing.austral.edu.ar>
2017-05-23 14:57:35 -0300

    Exercise 2 DONE, MUST HAVE A TEST

--- source/TP4/Exercises.java
+++ source/TP4/Exercises.java
@@ -21,7 +21,34 @@ public class Exercises implements  TP4 {
 
     @Override
     public double[] exercise2(double[][] coefficients, double[] independentTerms) {
-        return new double[0];
+        double[][] coefficients2 = makeDiagonalToOne(coefficients);
+        double[] independentTerms2 = independentTermsByMakeDiagonalToOne(coefficients,independentTerms);
+        double[] result = new double[independentTerms2.length];
+        for (int i=0;i<coefficients2.length;i++){
+            result[i] = independentTerms2[i];
+            for (int j=i-1; j>=0; j--){
+                result[i] -= (coefficients[i][j]*result[j]);
+            }
+        }
+        return result;
+    }
+
+    private double[] independentTermsByMakeDiagonalToOne(double[][] coefficients, double[] independentTerms) {
+        double[] result = independentTerms;
+        for (int i=0; i<coefficients.length;i++){
+            result[i] = result[i]/coefficients[i][i];
+        }
+        return result;
+    }
+
+    private double[][] makeDiagonalToOne(double[][] coefficients) {
+        double[][] result = coefficients;
+        for (int i=0; i<coefficients.length;i++){
+            for (int j=0; j<coefficients.length;j++){
+                result[i][j] = result[i][j]/result[i][i];
+            }
+        }
+        return result;
     }
 
     @Override
